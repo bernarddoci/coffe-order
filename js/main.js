2fcf53b2996c497bcdd40930fb4837b682acdb5d
bernard <bernard_doci@hotmail.com>
2015-02-17 12:49:04 +0100

    js added

--- js/main.js
+++ js/main.js
@@ -5,12 +5,7 @@ $(document).ready(function($) {
 	$age = $('#age');
 	$add = $('#add-friend');
 
-	var friendTemplate = ""+
-	"<li>" +
-	"<p><strong>Name: </strong>{{name}}</p>"+
-	"<p><strong>Age: </strong>{{age}}</p>"+
-	"<button data-id={{id}} class='remove'>Remove</button>"+
-	"</li>";
+	var friendTemplate = $('#friend-template').html();
 
 	function showFriends(friend){
 		$friends.append(Mustache.render(friendTemplate, friend));
@@ -63,8 +58,39 @@ $(document).ready(function($) {
 		});
 	});
 
+	$friends.delegate('.editFriend', 'click', function(){
+		var $li = $(this).closest('li');
 
+		$li.find('input.name').val($li.find('span.name').html());
+		$li.find('input.age').val($li.find('span.age').html());
+		$li.addClass('edit');
+	});
 
+	$friends.delegate('.cancleEdit', 'click', function(){
+		$(this).closest('li').removeClass('edit');
+	});
 
+	$friends.delegate('.saveFriend', 'click', function(){
+		var $li = $(this).closest('li');
+		var friend = {
+			name: $li.find('input.name').val(),
+			age: $li.find('input.age').val(),
+		}
+
+		$.ajax({
+			url: 'http://rest.learncode.academy/api/bernardo/friends/' + $(this).attr('data-id'),
+			type: 'PUT',
+			data: friend,
+			success: function(friend){
+				$li.find('span.name').html(friend.name);
+				$li.find('span.age').html(friend.age);
+				$li.removeClass('edit');
+			},
+			error: function(){
+				alert("error updatin order");
+			}
+		});
+		
+	});
 	
 });
